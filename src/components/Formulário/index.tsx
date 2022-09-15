f345c9f9b237b89cf70313ae445881bff845758e
laufoes <foeslaura@gmail.com>
2022-09-15 10:00:35 -0300

    preventDefault no Formulário

--- src/components/Formulário/index.tsx
+++ src/components/Formulário/index.tsx
@@ -2,17 +2,23 @@ import styles from './Formulario.module.scss';
 import Lista from './Lista';
 
 export default function Formulario() {
-    return(
-        <div className={styles.formulario}>
-            <h1 className={styles.formulario__titulo}>Organize seus certificados!</h1>
-            <form>
+    const enviaFormulario = (evento : React.FormEvent) => {
+        evento.preventDefault();
+    }
+
+return(
+    <div className={styles.formulario}>
+        <h1 className={styles.formulario__titulo}>Organize seus certificados!</h1>
+            <form onSubmit={enviaFormulario}>
                 <label htmlFor="certificado">Adicione um novo certificado</label>
                 <Lista />
                 <button
-                    className={styles.formulario__botao}>
-                    Salvar certificados
+                className={styles.formulario__botao}
+                type="submit"
+                >
+                Salvar certificados
                 </button>
             </form>
-        </div>
-        )
-    }
+    </div>
+    )
+}
